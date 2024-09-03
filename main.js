97bf2e2bd8824ffbda0eb7cf94a23032eeb62488
sourkr <sourav.kumar.all.2021@gmail.com>
2024-09-03 11:28:35 +0530

    update

--- main.js
+++ main.js
@@ -2,13 +2,7 @@ const desktop = document.querySelector('desktop')
 const startBtn = document.querySelector('#start')
 const startMenu = document.querySelector('start-menu')
 
-const applications = [
-    {
-        icon: 'browser.webp',
-        name: 'Browser',
-        start: startBrowser
-    }
-]
+
 
 
 startBtn.onclick = () => {
@@ -46,34 +40,7 @@ window.onclick = ev => {
 }
 
 
-function startBrowser(app) {
-    const win = new Window(600, 300)
-    
-    win.icon.src = app.icon
-    win.title.innerText = app.name
-    
-    const root = document.createElement('div')
-    const searchBar = document.createElement('input')
-    const iframe = document.createElement('iframe')
 
-    iframe.src = 'https://www.google.com/webhp?igu=1'
-    iframe.style.cssText = `
-        flex: 1;
-        border: none;
-    `
-    
-    root.style.cssText = `
-        position: relative;
-        width: 100%;
-        height: 100%;
-    `
-    
-    
-    root.append(searchBar, iframe)
-    win.append(root)
-
-    console.log(win)
-}
 
 desktop.oncontextmenu = ev => {
     contextmenu(ev.pageX, ev.pageY, [
